9c1f9862e51bc09a3d7b47a6413820e4316d5318
Sam Hill <senmhil@gmail.com>
2025-08-14 14:46:39 +0100

    Changing cluster

--- main.tf
+++ main.tf
@@ -1,93 +1,142 @@
-
-
-
-
-# Filter out local zones, which are not currently supported 
-# with managed node groups
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
-
 locals {
-  cluster_name = "test-eks-${random_string.suffix.result}"
+  cluster_name = "sandbox-vpc-eks-test"
 }
 
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
-module "vpc" {
+module "vpc_eks" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "6.0.1"
+  version = "5.18.1"
 
-  name = "test-vpc"
+  name = "eks-test"
 
-  cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  cidr                  = "10.20.0.0/19"
 
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  private_subnets = ["10.20.0.0/21", "10.20.8.0/21", "10.20.16.0/21"]
+  public_subnets  = ["10.20.24.0/23", "10.20.26.0/23", "10.20.28.0/23"]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  enable_vpn_gateway = true
+
   enable_dns_hostnames = true
+  enable_dns_support   = true
 
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
+  propagate_private_route_tables_vgw = true
+  propagate_public_route_tables_vgw  = true
 
   private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
-}
-
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "21.0.9"
-
-  name    = local.cluster_name
-
-  endpoint_public_access           = true
-  enable_cluster_creator_admin_permissions = true
-
-  addons = {
-    aws-ebs-csi-driver = {
-      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
-    }
+    "kubernetes.io/role/internal-elb" = "1",
+    "mapPublicIpOnLaunch"             = "FALSE"
+    "karpenter.sh/discovery"          = local.cluster_name
+    "kubernetes.io/role/cni"          = "1"
   }
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  compute_config = {
-    enabled    = true
-    node_pools = ["general-purpose"]
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1",
+    "mapPublicIpOnLaunch"    = "TRUE"
   }
 
   tags = {
-    Environment = "dev"
-    Terraform   = "true"
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+}
+
+resource "aws_eks_cluster" "cluster" {
+  name     = local.cluster_name
+  role_arn = aws_iam_role.cluster.arn
+  version  = "1.32"
+
+  vpc_config {
+    subnet_ids              = module.vpc_eks.private_subnets
+    security_group_ids      = []
+    endpoint_private_access = "true"
+    endpoint_public_access  = "true"
   }
 
+  access_config {
+    authentication_mode                         = "API"
+    bootstrap_cluster_creator_admin_permissions = false
+  }
+
+  bootstrap_self_managed_addons = false
+
+  zonal_shift_config {
+    enabled = true
+  }
+
+  compute_config {
+    enabled       = true
+    node_pools    = ["general-purpose", "system"]
+    node_role_arn = aws_iam_role.node.arn
+  }
+
+  kubernetes_network_config {
+    elastic_load_balancing {
+      enabled = true
+    }
+  }
+
+  storage_config {
+    block_storage {
+      enabled = true
+    }
+  }
 }
 
+resource "aws_iam_role" "cluster" {
+  name = "eks-test-cluster-role"
 
-# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/ 
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  assume_role_policy = data.aws_iam_policy_document.cluster_role_assume_role_policy.json
 }
 
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "5.39.0"
+resource "aws_iam_role_policy_attachments_exclusive" "cluster" {
+  role_name = aws_iam_role.cluster.name
+  policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSComputePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSBlockStoragePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSNetworkingPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSServicePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  ]
+}
 
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+data "aws_iam_policy_document" "cluster_role_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole", "sts:TagSession"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "node" {
+  name = "eks-auto-node-example"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = ["sts:AssumeRole"]
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodeMinimalPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodeMinimalPolicy"
+  role       = aws_iam_role.node.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryPullOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+  role       = aws_iam_role.node.name
 }

d11942ad9a71922c2667a8ef342dbd1a15859b53
Sam Hill <senmhil@gmail.com>
2025-08-15 08:59:59 +0100

    Trying different cluster

--- main.tf
+++ main.tf
@@ -49,13 +49,13 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.0.9"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.33"
+  name    = local.cluster_name
+  kubernetes_version = "1.33"
 
-  cluster_endpoint_public_access           = true
+  endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
 
-  cluster_addons = {
+  addons = {
     aws-ebs-csi-driver = {
       service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
     }
@@ -64,15 +64,9 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
-
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
-
       instance_types = ["t3.small"]
 
       min_size     = 1
